3e06a9a0c77bc3bb0f86e2f2b9f76132efe4f07b
Alejandro Robles <arobles2828@gmail.com>
2021-05-05 18:15:49 -0700

    changed

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'Jacerobles12@',
+  password: '',
   database: 'election'
 });
 
